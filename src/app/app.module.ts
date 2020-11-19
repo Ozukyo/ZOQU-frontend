29b39a99f6a7dc27eb26ba4fe500bca6c4d38089
Ozukyo <ozukyo@gmail.com>
2020-11-19 12:08:31 +0100

    Create empty component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RegisterFormComponent
   ],
   imports: [
     BrowserModule,
