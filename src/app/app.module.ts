85430c7b96f27bca28bbd22cbd0cf415949744db
Ozukyo <ozukyo@gmail.com>
2020-11-30 12:10:53 +0100

    Add photos grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SingleAnnouncmentViewComponent } from './single-announcment-view/single-announcment-view.component';
+import { PhotosViewComponent } from './single-announcment-view/photos-view/photos-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SingleAnnouncmentViewComponent,
+    PhotosViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/single-announcment-view/single-announcment-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-announcment-view',
+  templateUrl: './single-announcment-view.component.html',
+  styleUrls: ['./single-announcment-view.component.scss']
+})
+export class SingleAnnouncmentViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
