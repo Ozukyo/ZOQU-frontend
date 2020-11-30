0b3c8926134f4ae8ee29ac5a780c6fca6a632ca7
Ozukyo <ozukyo@gmail.com>
2020-11-30 20:47:33 +0100

    Add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,22 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { SingleAnnouncmentViewComponent } from './single-announcment-view/single-announcment-view.component';
-import { PhotosViewComponent } from './single-announcment-view/photos-view/photos-view.component';
-import { AnnouncmentInfoComponent } from './single-announcment-view/announcment-info/announcment-info.component';
+import { SingleAnnouncementViewComponent } from './single-announcement-view/single-announcement-view.component';
+import { PhotosViewComponent } from './single-announcement-view/photos-view/photos-view.component';
+import { AnnouncementInfoComponent } from './single-announcement-view/announcement-info/announcement-info.component';
+import {AnnouncementDetailsComponent} from './single-announcement-view/announcement-details/announcement-details.component';
+import { LeftSideViewComponent } from './single-announcement-view/left-side-view/left-side-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SingleAnnouncmentViewComponent,
+    SingleAnnouncementViewComponent,
     PhotosViewComponent,
-    AnnouncmentInfoComponent
+    AnnouncementInfoComponent,
+    AnnouncementDetailsComponent,
+    LeftSideViewComponent
   ],
   imports: [
     BrowserModule,
