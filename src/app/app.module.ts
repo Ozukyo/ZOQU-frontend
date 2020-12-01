49f23f847e332c122cb4b98c0925e0a5d1274286
Ozukyo <ozukyo@gmail.com>
2020-12-02 00:19:08 +0100

    Add personal info edit page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { PersonalInfoEditPageComponent } from './personal-info-edit-page/personal-info-edit-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    PersonalInfoEditPageComponent
   ],
   imports: [
     BrowserModule,
