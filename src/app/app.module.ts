bd290c6859f485a6fe0a0bff197aa2f1f92f0b09
Ozukyo <ozukyo@gmail.com>
2020-12-02 14:12:27 +0100

    Revert some changes, delete some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AnnouncmentsViewComponent } from './announcments-view/announcments-view.component';
-import { LoginFormComponent } from './login-form/login-form.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { SidebarCategoriesComponent } from './components/sidebar-categories/sidebar-categories.component';
-import { PersonalInfoEditPageComponent } from './personal-info-edit-page/personal-info-edit-page.component';
 import { ProfilePageHeaderComponent } from './profile-page-header/profile-page-header.component';
 import { ProfileContactbarComponent } from './profile-contactbar/profile-contactbar.component';
 import {ProfileAboutMeComponent} from './profile-about-me/profile-about-me.component';
@@ -20,11 +17,8 @@ import {ProfileAboutMeComponent} from './profile-about-me/profile-about-me.compo
     HeaderComponent,
     FooterComponent,
     AnnouncmentsViewComponent,
-    LoginFormComponent,
-    RegisterFormComponent,
     CategoriesComponent,
     SidebarCategoriesComponent,
-    PersonalInfoEditPageComponent,
     ProfilePageHeaderComponent,
     ProfileContactbarComponent,
     ProfileAboutMeComponent
