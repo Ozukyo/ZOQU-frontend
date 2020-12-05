9fc07555f0b7ec19b0b2276b34628f534c24e54b
Ozukyo <ozukyo@gmail.com>
2020-12-05 19:33:34 +0100

    Create adding-form and adding-announcement component

---
+++ src/app/adding-announcement-page/adding-form/adding-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adding-form',
+  templateUrl: './adding-form.component.html',
+  styleUrls: ['./adding-form.component.scss']
+})
+export class AddingFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AddingFormComponent } from './adding-announcement-page/adding-form/adding-form.component';
+import { AddingAnnouncementPageComponent } from './adding-announcement-page/adding-announcement-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AddingFormComponent,
+    AddingAnnouncementPageComponent
   ],
   imports: [
     BrowserModule,
