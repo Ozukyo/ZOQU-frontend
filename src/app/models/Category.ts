0fc6abbac59d7fd056e62c9c3e76226a72d2fb60
KozubIgn <ignacykozub@gmail.com>
2020-12-15 18:01:36 +0100

    refactor/load category from json-server

--- src/app/models/Category.ts
+++ src/app/models/Category.ts
@@ -1,8 +1,8 @@
 import {Subcategory} from './Subcategory';
 
 export interface Category {
-id: number;
-name: string;
-subcategory_id: number;
-subcategories: Subcategory[];
+  id: number;
+  name: string;
+  subcategory_id: number;
+  subcategories: Subcategory[];
 }
