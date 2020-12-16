39154aaa7ad42ba51096ca34afa0f64e19f73c81
KozubIgn <ignacykozub@gmail.com>
2020-12-16 11:35:59 +0100

    not done/ create routes form category name

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -10,12 +10,31 @@ import {CategoryService} from '../../services/category.service';
 })
 export class CategoriesComponent implements OnInit {
   categoriesList: Observable<Category[]>;
+  letters: any = {};
+  category: any = '';
 
   constructor(private categoryService: CategoryService) {
   }
 
   ngOnInit(): void {
+    const categoryName = this.category.name;
     this.categoriesList = this.categoryService.getCategories();
+    this.letters = {
+      261: 'a',
+      263: 'c',
+      281: 'e',
+      322: 'l',
+      324: 'n',
+      243: 'o',
+      347: 's',
+      378: 'z',
+      380: 'z'
+    };
+    Object.keys(this.letters).map((key, index) => {
+      console.log(this.letters[key]);
+    });
+    console.log(categoryName);
   }
 
+//   transformLetters()
 }
