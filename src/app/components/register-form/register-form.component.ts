2008cdca5187b38f96886e5d0c8f11b66e8b9c2f
Ozukyo <ozukyo@gmail.com>
2021-01-06 14:29:43 +0100

    Add confriming password validator

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-form.component.scss']
 })
 export class RegisterFormComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      surname: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      userPassword: new FormGroup({
+        password: new FormControl('', Validators.required),
+        confirmPassword: new FormControl('', Validators.required)
+      }, {validators: this.passwordCheck.bind(this)})
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.registerForm);
+  }
+
+  passwordCheck(group: FormGroup): { [s: string]: boolean } {
+    if (!(group.get('password').value === group.get('confirmPassword').value)) {
+      return {passwordNotTheSame: true};
+    }
+    return null;
   }
 
 }
