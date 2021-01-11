1186a7fecb478ede84139c8a34930a9e3cb22857
Ozukyo <ozukyo@gmail.com>
2021-01-11 19:51:45 +0100

    Add loadSubcategories

--- src/app/components/adding-announcement-page/adding-form/adding-form.component.ts
+++ src/app/components/adding-announcement-page/adding-form/adding-form.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Category} from '../../../models/Category';
+import {CategoryService} from '../../../services/category.service';
+import {Subcategory} from '../../../models/Subcategory';
 
 @Component({
   selector: 'app-adding-form',
@@ -8,12 +11,20 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class AddingFormComponent implements OnInit {
   addingForm: FormGroup;
+  titleChars = 70;
+  contentChars = 7000;
+  categories: Category[];
+  // isCategorySelected = false;
+  subcategories: Subcategory[];
 
-  constructor() {
+  constructor(private categoryService: CategoryService) {
+  }
+
+  ngOnInit(): void {
     this.addingForm = new FormGroup({
       title: new FormControl('', [Validators.required, Validators.maxLength(70)]),
-      category: new FormControl(''),
-      content: new FormControl(''),
+      category: new FormControl('ONA'),
+      content: new FormControl('', Validators.maxLength(7000)),
       productDetails: new FormGroup({
         color: new FormControl(''),
         material: new FormControl(''),
@@ -21,11 +32,14 @@ export class AddingFormComponent implements OnInit {
       productPictures: new FormGroup({
         picture: new FormControl()
       }),
-      price: new FormControl(''),
+      price: new FormControl('', [Validators.required, Validators.maxLength(5)]),
     });
-  }
 
-  ngOnInit(): void {
+    this.getCategories();
+
+    // if (this.addingForm.get('category').value.val) {
+    //   this.loadSubcategories(this.addingForm.get('category').value);
+    // }
   }
 
   activateInput(elem: any): void {
@@ -34,5 +48,23 @@ export class AddingFormComponent implements OnInit {
 
   onSubmit(): void {
     console.log(this.addingForm);
+    console.log(this.addingForm.get('title').value.length);
   }
+
+  getCategories(): void {
+    this.categoryService.getCategories()
+      .subscribe(data => {
+        this.categories = data;
+      });
+  }
+
+  loadSubcategories(nameOfCategory: string): void {
+    this.categoryService.getCategoryByName(nameOfCategory)
+      .subscribe(category => {
+        this.subcategories = category.subcategories;
+        console.log(category);
+        console.log(category.subcategories);
+      });
+  }
+
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Category} from '../models/Category';
+import {map} from 'rxjs/operators';
 
 
 @Injectable({
@@ -23,5 +24,16 @@ export class CategoryService {
   getCategory(announcement: Category): Observable<Category> {
     const url = `${this.categoriesURL}/${announcement.id}`;
     return this.http.get<Category>(url);
+  }
+
+  getCategoryByName(name: string): Observable<Category> {
+    return this.http.get<Category[]>(this.categoriesURL)
+      .pipe(map(categoryList => {
+        for (const category of categoryList) {
+          if (category.name === name) {
+            return category;
+          }
+        }
+      }));
   }
 }
