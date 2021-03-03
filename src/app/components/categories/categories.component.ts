fb577698b1851d1029673d0bdaa7fe210baad9b4
KozubIgn <ignacykozub@gmail.com>
2021-03-03 12:32:16 +0100

    fix/ feature display grouped categories from database

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,10 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {Category} from '../../models/Category';
-import {Observable, pipe} from 'rxjs';
+import {Observable} from 'rxjs';
 import {CategoryService} from '../../services/category.service';
-import {map, tap} from 'rxjs/operators';
-import {CategoryData} from '../../models/CategoryData';
+import {tap} from 'rxjs/operators';
 import {ICategoryDataDto} from '../../models/interfaces/ICategoryDataDto';
+import {CategoryData} from '../../models/CategoryData';
 
 @Component({
   selector: 'app-categories',
@@ -12,61 +11,13 @@ import {ICategoryDataDto} from '../../models/interfaces/ICategoryDataDto';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
-  categoriesList: Observable<Category[]>;
-  letters: any = {};
-  isPolish = false;
-  mainCategory = '';
-  mainCategoriesList: Observable<ICategoryDataDto[]>;
+
+  mainCategoriesList: Observable<CategoryData[]>;
 
   constructor(private categoryService: CategoryService) {
   }
 
   ngOnInit(): void {
-    this.mainCategoriesList = this.categoryService.getMainCategories().pipe(tap(console.log));
-
-
-    //   this.categoriesList = this.categoryService.getCategories();
-    //   this.letters = {
-    //     261: 'a',
-    //     263: 'c',
-    //     281: 'e',
-    //     322: 'l',
-    //     324: 'n',
-    //     243: 'o',
-    //     347: 's',
-    //     378: 'z',
-    //     380: 'z'
-    //   };
-    //
-    // }
-    //
-    // public convertLettersToEnglish(word: any): string {
-    //   this.toggleIfPolishLetter(word);
-    //
-    //   if (word.includes(', ')) {
-    //     word = word.split(', ').join('-');
-    //   } else if (word.includes(' ')) {
-    //     word = word.split(' ').join('-');
-    //   }
-    //
-    //   if (!this.isPolish) {
-    //     return word;
-    //   }
-    //
-    //   for (let i = 0; i < word.length; i++) {
-    //     for (const [key, value] of Object.entries(this.letters)) {
-    //       if (word[i] === String.fromCharCode(parseInt(key, 10))) {
-    //         word = word.replace(word[i], value);
-    //       }
-    //     }
-    //   }
-    //   return word;
-    //
-    // }
-    //
-    // public toggleIfPolishLetter(word: any): void {
-    //   this.isPolish = ['ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż'].some(letter => word.includes(letter));
-    // }
-
+    this.mainCategoriesList = this.categoryService.getAllCategoriesGroupedByLevel().pipe(tap(console.log));
   }
 }
--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,65 +1,50 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import {Category} from '../models/Category';
 import {CategoryData} from '../models/CategoryData';
 import {ICategoryDataDto} from '../models/interfaces/ICategoryDataDto';
 import {environment} from '../../environments/environment';
 import {map} from 'rxjs/operators';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
-  // categoriesURL = 'http://localhost:3000/categories';
-  // httpOptions = {
-  //   headers: new HttpHeaders({'Content-Type': 'application/json'})
-  // };
-  // categoryList: CategoryData[];
 
   constructor(private http: HttpClient) {
   }
 
-  // getCategories(): Observable<Category[]> {
-  //   return this.http.get<Category[]>(this.categoriesURL);
-  // }
-
-  getMainCategories(): Observable<CategoryData[]> {
-    // const newCategoryDataList: CategoryData[] = [];
+  getAllCategoriesGroupedByLevel(): Observable<CategoryData[]> {
     return this.http.get<ICategoryDataDto[]>(`${environment.apiUrl}categories`)
       .pipe(map(categories => {
 
-        const level1Dto = [];
-        const level2Dto = [];
-        const level3Model = [];
-        categories.forEach(dataItem => {
-          switch (dataItem.category_level) {
+        const firstLvlCategoriesDto = [];
+        const secondLvlCategoriesDto = [];
+        const thirdLvlCategoriesDto = [];
+        categories.forEach(category => {
+          switch (category.category_level) {
             case 1:
-              level1Dto.push(dataItem);
+              firstLvlCategoriesDto.push(category);
               break;
             case 2:
-              level2Dto.push(dataItem);
+              secondLvlCategoriesDto.push(category);
               break;
             case 3:
-              level3Model.push(new CategoryData(dataItem));
+              thirdLvlCategoriesDto.push(new CategoryData(category));
               break;
           }
         });
-        const level2Model = this.convertCategoryDtoToModel(level2Dto, level3Model);
-        return this.convertCategoryDtoToModel(level1Dto, level2Model);
+        const level2Model = this.convertCategoryDtoToModel(secondLvlCategoriesDto, thirdLvlCategoriesDto);
+        return this.convertCategoryDtoToModel(firstLvlCategoriesDto, level2Model);
       }));
   }
-  private convertCategoryDtoToModel(categoryDto: ICategoryDataDto[], subcategories: CategoryData[]): CategoryData[]{
-     return categoryDto.map(item => {
+
+  private convertCategoryDtoToModel(categoryDto: ICategoryDataDto[], subcategories: CategoryData[]): CategoryData[] {
+    return categoryDto.map(item => {
       const model = new CategoryData(item);
-      model.subcategories = subcategories.filter(i => i.parentId === model.id);
+      model.subcategories = subcategories.filter(subcategory => subcategory.parentId === model.id);
       return model;
     });
   }
-  //
-  // getCategory(announcement: Category): Observable<Category> {
-  //   const url = `${this.categoriesURL}/${announcement.id}`;
-  //   return this.http.get<Category>(url);
-  // }
 }
+
