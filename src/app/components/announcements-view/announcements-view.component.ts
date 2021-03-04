88c2387e91716633a35b883dc820a68a23e0e534
KozubIgn <ignacykozub@gmail.com>
2021-03-04 16:07:55 +0100

    fix typo

--- src/app/components/announcements-view/announcements-view.component.ts
+++ src/app/components/announcements-view/announcements-view.component.ts
@@ -5,7 +5,7 @@ import {AnnouncementData} from '../../models/AnnouncementData';
 import {tap} from 'rxjs/operators';
 
 @Component({
-  selector: 'app-announcments-view',
+  selector: 'app-announcements-view',
   templateUrl: './announcements-view.component.html',
   styleUrls: ['./announcements-view.component.scss']
 })
@@ -18,6 +18,5 @@ export class AnnouncementsViewComponent implements OnInit {
 
   ngOnInit(): void {
     this.announcementList = this.announcementService.getAnnouncements().pipe(tap(console.log));
-
   }
 }
