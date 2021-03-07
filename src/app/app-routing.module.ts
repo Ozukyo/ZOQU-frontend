093b086ccda753d709f85a6b1ddc1ef0696fd2e0
KozubIgn <ignacykozub@gmail.com>
2021-03-07 14:55:19 +0100

    update angular CLI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -153,7 +153,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 
