801d3cffb81d9256b7773373d2fd6aae2c626f00
KozubIgn <ignacykozub@gmail.com>
2021-03-10 07:41:58 +0100

    add announcement info into component

--- src/app/components/single-announcement-view/single-announcement-view.component.ts
+++ src/app/components/single-announcement-view/single-announcement-view.component.ts
@@ -1,30 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {AnnouncementData} from '../../models/AnnouncementData';
-import {AnnouncementService} from '../../services/announcement.service';
-import {ActivatedRoute, Router} from '@angular/router';
-
-import {tap} from 'rxjs/operators';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-single-announcement-view',
   templateUrl: './single-announcement-view.component.html',
   styleUrls: ['./single-announcement-view.component.scss']
 })
-export class SingleAnnouncementViewComponent implements OnInit {
+export class SingleAnnouncementViewComponent {
 
-  id: number;
-  announcement: AnnouncementData;
-
-  constructor(private activatedRoute: ActivatedRoute,
-              private router: Router,
-              private announcementService: AnnouncementService) {
-  }
-
-  ngOnInit(): void {
-    this.activatedRoute.url.subscribe(myUrl => this.id = +myUrl[0].path);
-    console.log(this.id);
-    this.announcementService.getAnnouncementById(this.id)
-      .pipe(tap(console.log))
-      .subscribe(data => this.announcement = data);
+  constructor() {
   }
 }
