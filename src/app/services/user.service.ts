da2b50a819e3fa0e74775da673d460acac64da89
KozubIgn <ignacykozub@gmail.com>
2021-03-11 20:43:45 +0100

    getUserByemail method

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,19 +2,44 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {UserData} from '../models/UserData';
 import {environment} from '../../environments/environment';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
+import {IUserDataDto} from '../models/interfaces/IUserDataDto';
+import {map} from 'rxjs/operators';
+import {RegisterUserData} from '../models/RegisterUserData';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class UserService {
+  userEmail = '';
 
   constructor(private http: HttpClient) {
-
+this.userEmail = localStorage.getItem('email');
   }
 
-  addUser(user: UserData): Observable<UserData> {
-    return this.http.post<UserData>(`${environment.apiUrl}users`, user);
+  addUser(user: RegisterUserData): Observable<RegisterUserData> {
+    return this.http.post<RegisterUserData>(`${environment.apiUrl}users`, user);
   }
+
+  getUserByEmail(userEmail: string): Observable<UserData> {
+    return this.http.get<IUserDataDto>(`${environment.apiUrl}users/email/${userEmail}`).pipe(map(data => {
+        // console.log(data);
+        return new UserData(data);
+      }
+    ));
+  }
+  saveEmailToLocalStorage(): void {
+    localStorage.setItem('email', this.userEmail);
+  }
+
+  updateUserEmail(email: string): void {
+    this.userEmail = email;
+  }
+
+  getUserEmail(): Observable<string> {
+    console.log(this.userEmail);
+    return of(this.userEmail);
+  }
+
 }
