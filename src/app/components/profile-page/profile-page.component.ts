a36fd0aee0cfffd18cdab7f549ed94cb77b92950
KozubIgn <ignacykozub@gmail.com>
2021-03-11 20:44:41 +0100

    get user method

--- src/app/components/profile-page/profile-page.component.ts
+++ src/app/components/profile-page/profile-page.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UserService} from '../../services/user.service';
+import {Router} from '@angular/router';
+import {UserData} from '../../models/UserData';
+import {switchMap, take, tap} from 'rxjs/operators';
+import {log} from 'util';
 
 @Component({
   selector: 'app-profile-page',
@@ -6,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent implements OnInit {
+  user: UserData;
+  email: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private userService: UserService) {
   }
 
+  ngOnInit(): void {
+    this.getUser();
+    console.log(this.user);
+  }
+
+  private getUser(): void {
+    this.userService.getUserEmail().subscribe(data => this.email = data);
+    console.log(this.email);
+    this.userService.getUserByEmail(this.email).subscribe(data => this.user = data);
+  }
 }
