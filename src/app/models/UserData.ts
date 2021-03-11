f0c2198118a1715da58912829d2653f58578d773
KozubIgn <ignacykozub@gmail.com>
2021-03-11 20:42:32 +0100

    add userData property

--- src/app/models/UserData.ts
+++ src/app/models/UserData.ts
@@ -1,19 +1,24 @@
 import {IUserDataDto} from './interfaces/IUserDataDto';
 
 export class UserData {
-  // id: number;
+  id: number;
+  email: string;
   firstname: string;
   lastname: string;
-  email: string;
+  phonenumber: string;
+  description: string;
+  webpage: string;
   password: string;
 
   constructor(input: IUserDataDto) {
-    // this.id = input.id;
+    this.id = input.id;
     this.firstname = input.first_name;
     this.lastname = input.last_name;
     this.email = input.email;
+    this.phonenumber = input.phone_number;
+    this.webpage = input.web_page;
+    this.description = input.description;
     this.password = input.password;
   }
-
 }
 
