6c167833a5eeb461d4d491ab0ded773af0cb983b
KozubIgn <ignacykozub@gmail.com>
2021-03-17 15:54:52 +0100

    refactor/remove setTimeout

--- src/app/components/single-announcement-view/announcement-info/announcement-info.component.ts
+++ src/app/components/single-announcement-view/announcement-info/announcement-info.component.ts
@@ -1,8 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AnnouncementService} from '../../../services/announcement.service';
 import {AnnouncementData} from '../../../models/AnnouncementData';
 import {switchMap, take, tap} from 'rxjs/operators';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
+import {UserService} from '../../../services/user.service';
+import {UserData} from '../../../models/UserData';
 
 @Component({
   selector: 'app-announcement-info',
@@ -11,14 +13,18 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class AnnouncementInfoComponent implements OnInit {
 
-announcement: AnnouncementData;
+  announcement: AnnouncementData;
   id: number;
+  user: UserData;
 
   constructor(private  announcementService: AnnouncementService,
-              private activatedRoute: ActivatedRoute) {
+              private activatedRoute: ActivatedRoute,
+              private userService: UserService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
+
     this.activatedRoute.url.pipe(
       tap(myUrl => this.id = +myUrl[0].path),
       switchMap(() => this.announcementService.getAnnouncementById(this.id)),
@@ -27,6 +33,13 @@ announcement: AnnouncementData;
       this.announcement = data;
       console.log(data);
     });
+    setTimeout(() => {
+      this.userService.getUserById(this.announcement.userId).subscribe(data => {
+        this.user = data;
+        console.log(data);
+      });
+    }, 500);
+
 
     // this.activatedRoute.url.pipe(
     //   map(myUrl => {
@@ -42,4 +55,7 @@ announcement: AnnouncementData;
 
   }
 
+  onNavigate(): void{
+    this.router.navigate([`profil/${this.user.id}`]);
+  }
 }
